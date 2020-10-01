da84a654370f956d2211b23cf6d5b25af3cbc6c8
John Royce <royce@royce-iMac.local>
2020-10-01 13:57:32 +0800

    train multiple models

--- data-mining/school/module1/r/multi-models/model.R
+++ data-mining/school/module1/r/multi-models/model.R
@@ -3,24 +3,48 @@ library(caret)
 library(randomForest)
 data(PimaIndiansDiabetes)
 
-# prepare training scheme
-trainControl <- trainControl(method="repeatedcv", number=10, repeats=3) 
+
+get_train_control <- function(type, no_fold, no_repeats) {
+    control <- trainControl(method="repeatedcv", number=10, repeats=3) 
+    return(control)
+}
+
+generate_seed <- function(n) {
+    set.seed(n)
+    return(TRUE)
+}
+
+train_model <- function(type, control, y_train, data_sets) {
+    model <- train(y_train ~ ., data=data_sets, method=type, trContro=control)
+    return(model)
+}
+
+CONTROL <- get_train_control("repeatedcv", 10, 3)
+Y_TRAIN <- diabetes
+DATA_SETS <- PimaIndiansDiabetes
 # CART
-set.seed(7)
-fit.cart <- train(diabetes~., data=PimaIndiansDiabetes, method="rpart", trControl=trainControl)
+generate_seed(7)
+cart.model <- train_model("rpart", CONTROL, Y_TRAIN, DATA_SETS)
+
 # LDA
-set.seed(7)
-fit.lda <- train(diabetes~., data=PimaIndiansDiabetes, method="lda", trControl=trainControl) 
+generate_seed(7)
+lda.model <- train_model("lda", CONTROL, Y_TRAIN, DATA_SETS)
+
 # SVM
-set.seed(7)
-fit.svm <- train(diabetes~., data=PimaIndiansDiabetes, method="svmRadial",trControl=trainControl)
-# KNN
-set.seed(7)
-fit.knn <- train(diabetes~., data=PimaIndiansDiabetes, method="knn", trControl=trainControl) 
+generate_seed(7)
+svm.model <- train_model("svmRadial", CONTROL, Y_TRAIN, DATA_SETS)
+
+#KNN
+generate_seed(7)
+knn.model <- train_model("knn", CONTROL, Y_TRAIN, DATA_SETS)
+
 # Random Forest
-set.seed(7)
 fit.rf <- train(diabetes~., data=PimaIndiansDiabetes, method="rf", trControl=trainControl)
+generate_seed(7)
+rf.model <- train_model("rf", CONTROL, Y_TRAIN, DATA_SETS)
+
 # collect resamples
-results <- resamples(list(CART=fit.cart, LDA=fit.lda, SVM=fit.svm, KNN=fit.knn, RF=fit.rf))
+results <- resamples(list(CART=cart.model, LDA=lda.model, SVM=svm.model, KNN=knn.model, RF=rf.model))
 
 summary(results)
+
