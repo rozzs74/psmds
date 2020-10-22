fe4ac2db3939b2666aeb7515d78a6a097ad1baef
John Royce <royce@royce-iMac.local>
2020-10-23 05:51:57 +0800

    add density and whisker plots

--- machine-learning/books/machine-learning-mastery-with-Python/chapter6/understand_data_with_visualization.py
+++ machine-learning/books/machine-learning-mastery-with-Python/chapter6/understand_data_with_visualization.py
@@ -8,12 +8,21 @@ def main():
 
 	columns = ["preg", "plas", "pres", "skin", "test", "mass", "pedi", "age", "class"]
 	df = read_csv(path, names=columns)
-
-	show_histogram(df)
+	# show_histogram(df)
+	# show_density_plots(df)
+	show_whisker_plots(df)
 
 
 def show_histogram(df):
 	df.hist()
+	show_plot()
+
+def show_density_plots(df):
+	df.plot(kind="density", subplots=True, layout=(3, 3), sharex=False)
+	show_plot()
+
+def show_whisker_plots(df):
+	df.plot(kind="box", subplots=True, layout=(3, 3), sharex=False, sharey=False)
 	show_plot()
 
 def show_plot():
