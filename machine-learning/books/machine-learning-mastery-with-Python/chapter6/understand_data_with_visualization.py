1abe5fef7f1eaec6f507f729d2e71f9a3929ec7c
John Royce <royce@royce-iMac.local>
2020-10-23 22:40:29 +0800

    completed chapter6

--- machine-learning/books/machine-learning-mastery-with-Python/chapter6/understand_data_with_visualization.py
+++ machine-learning/books/machine-learning-mastery-with-Python/chapter6/understand_data_with_visualization.py
@@ -2,6 +2,8 @@
 from pandas import read_csv
 from pandas import set_option
 from matplotlib import pyplot
+from pandas.plotting import scatter_matrix
+import numpy
 
 def main():
 	path = "../../machine-learning-mastery-with-Python/pima-indians-diabetes.data.csv"
@@ -10,7 +12,10 @@ def main():
 	df = read_csv(path, names=columns)
 	# show_histogram(df)
 	# show_density_plots(df)
-	show_whisker_plots(df)
+	# show_whisker_plots(df)
+	# df_corr = get_correlation(df)
+	# show_correlation_plot(df_corr, columns)
+	show_scatter_plot(df)
 
 
 def show_histogram(df):
@@ -25,8 +30,27 @@ def show_whisker_plots(df):
 	df.plot(kind="box", subplots=True, layout=(3, 3), sharex=False, sharey=False)
 	show_plot()
 
+def show_correlation_plot(correlations, names):
+	fig = pyplot.figure()
+	ax = fig.add_subplot(111)
+	cax = ax.matshow(correlations, vmin=-1, vmax=1)
+	fig.colorbar(cax)
+	ticks = numpy.arange(0, 9, 1)
+	ax.set_xticks(ticks)
+	ax.set_yticks(ticks)
+	ax.set_xticklabels(names) 
+	ax.set_yticklabels(names)
+	show_plot()
+
+def show_scatter_plot(df):
+	scatter_matrix(df)
+	show_plot()
+
 def show_plot():
 	pyplot.show()
+
+def get_correlation(df):
+	return df.corr()
 
 
 if __name__ == "__main__":
