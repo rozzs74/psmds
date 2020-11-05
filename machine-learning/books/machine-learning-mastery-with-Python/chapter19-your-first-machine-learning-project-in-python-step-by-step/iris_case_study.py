14f5720f6b9ee877fc6e66a09964847ffd83abec
John Royce <royce@royce-iMac.local>
2020-11-05 16:22:01 +0800

    completed chapter 19

--- machine-learning/books/machine-learning-mastery-with-Python/chapter19-your-first-machine-learning-project-in-python-step-by-step/iris_case_study.py
+++ machine-learning/books/machine-learning-mastery-with-Python/chapter19-your-first-machine-learning-project-in-python-step-by-step/iris_case_study.py
@@ -18,6 +18,58 @@ def main():
 	PATH = "../iris.csv"
 	columns = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
 	df = read_csv(PATH, names=columns)
+	df_dimension = df.shape
+	df_head = df.head(5)
+	df_summary = df.describe()
+	df_class_distribution = df.groupby("class").size()
+	
+	# Univariate plots
+	# df.plot(kind="box", subplots=True, layout=(2, 2), sharex=False, sharey=False)
+	# pyplot.show()
+	# df.hist()
+	# pyplot.show()
+	# End univariate plots
+
+	# Multivariate Plots
+	# scatter_matrix(df)
+	# pyplot.show()
+	
+	array = df.values
+	X = array[:, 0 : 4]
+	Y = array[:, 4]
+
+
+	# Spot-Check Algorithm
+	# models = [("LR", LogisticRegression(solver="lbfgs", max_iter=1000)), ("LDA", LinearDiscriminantAnalysis()), ("KNN", KNeighborsClassifier()), ("CART", DecisionTreeClassifier()), ("NB", GaussianNB()), ("SVM", SVC())]
+
+	# model_results = []
+	# model_names = []
+
+	# i = 0
+
+	# while i < len(models):
+	# 	el = models[i]
+	# 	kfold = KFold(n_splits=10, random_state=7, shuffle=True)
+	# 	cv_results = cross_val_score(el[1], X_train, Y_train, cv=kfold, scoring="accuracy")
+	# 	print(f"model: {el[0]}, results: mean={cv_results.mean() * 100:.3f} std={cv_results.std()*100:.3f}")
+	# 	model_results.append(cv_results)
+	# 	model_names.append(el[0])
+	# 	i += 1
+	# 	if i == len(models):
+	# 		# fig = pyplot.figure()
+	# 		# fig.suptitle("Algorithm Comparison")
+	# 		# ax = fig.add_subplot(111)
+	# 		# pyplot.boxplot(model_results)
+	# 		# ax.set_xticklabels(model_names)
+	# 		# pyplot.show()
+	# 		break
+	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=7)
+	m_knn = KNeighborsClassifier()
+	m_knn.fit(X_train, Y_train)
+	predictions = m_knn.predict(X_test)
+	print(f"Accuracy {accuracy_score(Y_test, predictions) * 100:.2f}%")
+	print(f"Confusion Matrix {confusion_matrix(Y_test, predictions)}")
+	print(f"Classification report {classification_report(Y_test, predictions)}")
 
 if __name__ == "__main__":
 	main()
