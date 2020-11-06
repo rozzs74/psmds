1352143ac1cf13cbd582d0bb9535974f8018b14b
John Royce <royce@royce-iMac.local>
2020-11-06 21:43:16 +0800

    completed chapter 20

--- machine-learning/books/machine-learning-mastery-with-Python/chapter20-regression-machine-learning-case-study-project/boston_house_regression.py
+++ machine-learning/books/machine-learning-mastery-with-Python/chapter20-regression-machine-learning-case-study-project/boston_house_regression.py
@@ -48,7 +48,144 @@ def main():
 	# pyplot.show()
 
 	# Multimodal Visualization
-	scatter_matrix(df)
-	pyplot.show()
+	# scatter_matrix(df)
+	# pyplot.show()
+	
+	# fig = pyplot.figure()
+	# ax = fig.add_subplot(111)
+	# cax = ax.matshow(df_corr, vmin=-1, vmax=1, interpolation="none")
+	# fig.colorbar(cax)
+	# ticks = numpy.arange(0, 14, 1)
+	# ax.set_xticks(ticks)
+	# ax.set_yticks(ticks)
+	# ax.set_xticklabels(columns)
+	# ax.set_yticklabels(columns)
+	# pyplot.show()
+
+	# Split-out validation dataset
+	array = df.values
+	X = array[:,0:13]
+	Y = array[:,13]
+	X_train, X_test, Y_train, Y_test =  train_test_split(X, Y, test_size=0.20, random_state=7)
+
+	# Spot-check algorithm baseline
+	# models = [("LR", LinearRegression()), ("LASSO", Lasso()), ("EN", ElasticNet()), ("KNN", KNeighborsRegressor()), ("CART", DecisionTreeRegressor()), ("SVR", SVR())]
+	# i = 0
+	# model_results = []
+	# model_name = []
+	# while i < len(models):
+	# 	el = models[i]
+	# 	kfold = KFold(n_splits=10, random_state=7, shuffle=True)
+	# 	cv_results = cross_val_score(el[1], X_train, Y_train, cv=kfold, scoring="neg_mean_squared_error")
+	# 	model_results.append(cv_results)
+	# 	model_name.append(el[0])
+	# 	print(el[0])
+	# 	print(f"Mean Estimated Accuracy: {cv_results.mean()}")
+	# 	print(f"STD: {cv_results.std()}")
+	# 	i += 1
+	# 	if i == len(models):
+	# 		fig = pyplot.figure()
+	# 		fig.suptitle("Algorithm Comparison")
+	# 		ax = fig.add_subplot(111)
+	# 		pyplot.boxplot(model_results)
+	# 		ax.set_xticklabels(model_name)
+	# 		pyplot.show()
+
+	# Spot-check algorithm with Standardization
+	# pipelines = [
+	# 	('ScaledLR', Pipeline([('Scaler', StandardScaler()),('LR', LinearRegression())])),
+	# 	('ScaledLASSO', Pipeline([('Scaler', StandardScaler()),('LASSO',Lasso())])),
+	# 	('ScaledEN', Pipeline([('Scaler', StandardScaler()),('EN', ElasticNet())])),
+	# 	('ScaledKNN', Pipeline([('Scaler', StandardScaler()),('KNN', KNeighborsRegressor())])),
+	# 	('ScaledCART', Pipeline([('Scaler', StandardScaler()),('CART', DecisionTreeRegressor())])),
+	# 	('ScaledSVR', Pipeline([('Scaler', StandardScaler()),('SVR', SVR())]))
+	# ]
+	# model_results = []
+	# model_name = []
+	# i = 0
+	# while i < len(pipelines):
+	# 	el = pipelines[i]
+	# 	kfold = KFold(n_splits=10, random_state=7, shuffle=True)
+	# 	cv_results = cross_val_score(el[1], X_train, Y_train, cv=kfold, scoring="neg_mean_squared_error")
+	# 	model_results.append(cv_results)
+	# 	model_name.append(el[0])
+	# 	print(f"name {el[0]} score: {cv_results.mean()} {cv_results.std()}")
+	# 	i += 1
+	# 	if i == len(pipelines):
+	# 		fig = pyplot.figure()
+	# 		fig.suptitle("Algorithm Comparison")
+	# 		ax = fig.add_subplot(111)
+	# 		pyplot.boxplot(model_results)
+	# 		ax.set_xticklabels(model_name)
+	# 		pyplot.show()
+	# 		break
+
+	# # Improve results with Tuning
+	# scaler = StandardScaler().fit(X_train)
+	# rescaledX = scaler.transform(X_train)
+	# k_values = numpy.array([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21])
+	# param_grid = dict(n_neighbors=k_values)
+	# model = KNeighborsRegressor()
+	# kfold = KFold(n_splits=10, random_state=7, shuffle=True)
+	# grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring="neg_mean_squared_error", cv=kfold)
+	# grid_result = grid.fit(rescaledX, Y_train)
+	# print(f"Best k {grid_result.best_score_} {grid_result.best_params_}")
+	# means = grid_result.cv_results_["mean_test_score"]
+	# stds = grid_result.cv_results_["std_test_score"]
+	# params = grid_result.cv_results_["params"]
+	# for mean, stdev, param in zip(means, stds, params):
+	# 	print(f"mean={mean} stdev={stdev} param={param}")
+
+	# Ensemble
+	# ensembles = [
+	# 	('ScaledAB', Pipeline([('Scaler', StandardScaler()),('AB',AdaBoostRegressor())])),
+	# 	('ScaledGBM', Pipeline([('Scaler', StandardScaler()),('GBM',GradientBoostingRegressor())])),
+	# 	('ScaledRF', Pipeline([('Scaler', StandardScaler()),('RF', RandomForestRegressor())])),
+	# 	('ScaledET', Pipeline([('Scaler', StandardScaler()),('ET',ExtraTreesRegressor())]))
+	# ]
+	# ensemble_results = []
+	# ensemble_name = []
+
+	# i = 0
+	# while i <= len(ensembles):
+	# 	el = ensembles[i]
+	# 	kfold = KFold(n_splits=10, random_state=7, shuffle=True)
+	# 	cv_results = cross_val_score(el[1], X_train, Y_train, cv=kfold, scoring="neg_mean_squared_error")
+	# 	ensemble_results.append(cv_results)
+	# 	ensemble_name.append(el[0])
+	# 	print(f"Name={el[0]} Mean={cv_results.mean()} STD={cv_results.std()}")
+	# 	i += 1
+	# 	if i == len(ensembles):
+	# 		fig = pyplot.figure()
+	# 		fig.suptitle("Algorithm Comparison Ensembles")
+	# 		ax = fig.add_subplot(111)
+	# 		pyplot.boxplot(ensemble_results)
+	# 		ax.set_xticklabels(ensemble_name)
+	# 		pyplot.show()
+	# 		break
+
+	# Tune ensembe method
+	# scaler = StandardScaler().fit(X_train)
+	# rescaledX = scaler.transform(X_train)
+	# param_grid = dict(n_estimators=numpy.array([10, 50, 100, 1000, 5000]))
+	# model = ExtraTreesRegressor()
+	# grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring="neg_mean_squared_error")
+	# grid_result = grid.fit(rescaledX, Y_train)
+	# print(f"Best score={grid_result.best_score_} param={grid_result.best_params_}")
+	# means = grid_result.cv_results_["mean_test_score"]
+	# std = grid_result.cv_results_["std_test_score"]
+	# params = grid_result.cv_results_["params"]
+
+	# for mean, stdev, param in zip(means, std, params):
+		# print(f"mean={mean}, std={stdev}, params={param }")
+	
+	# Finalize model
+	scaler = StandardScaler().fit(X_train)
+	rescaledX = scaler.transform(X_train)
+	model = ExtraTreesRegressor(random_state=7, n_estimators=1000)
+	model.fit(rescaledX, Y_train)
+	rescaledTestX = scaler.transform(X_test)
+	predictions = model.predict(rescaledTestX)
+	print(mean_squared_error(Y_test, predictions))
 if __name__ == "__main__":
 	main()
