6ac4dd0b57e27960c3d45ab8324a40fa3828f612
John Royce <royce@royce-iMac.local>
2021-02-09 17:09:43 +0800

    added data explorer

--- data-visualization/stream_lit/data_explorer_app/uber_pickups.py
+++ data-visualization/stream_lit/data_explorer_app/uber_pickups.py
@@ -13,11 +13,34 @@ st.title("Uber pickups in NYC")
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/''streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-
-
-
+@st.cache
 def load_data(nrows):
 	df = pd.read_csv(DATA_URL, nrows=nrows)
+	lowercase = lambda x: str(x).lower()
+	df.rename(lowercase, axis='columns', inplace=True)
+	df[DATE_COLUMN] = pd.to_datetime(df[DATE_COLUMN])
 	return df
 
-# https://docs.streamlit.io/en/stable/tutorial/create_a_data_explorer_app.html
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+df = load_data(1000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache)")
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(df)
+
+# Histogram
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(df[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+#Data Map
+st.subheader('Map of all pickups')
+st.map(df)
+
+hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+filtered_data = df[df[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
